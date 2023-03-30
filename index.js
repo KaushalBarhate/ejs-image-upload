46e705cd695ab1a3b92699e9e7c15e02418a262a
Kaushal Barhate <barhate.kaushal@gmail.com>
2023-03-30 19:41:11 +0530

    initial

--- index.js
+++ index.js
@@ -95,3 +95,4 @@ app.delete('/upload/:filename', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports = app
